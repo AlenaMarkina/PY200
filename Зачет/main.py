8c618a8b1edf668a2cc8110ff1ab9f1035f9812c
Alena Markina <markina.a7@yandex.ru>
2023-02-08 20:58:34 +0300

    Продолжаю работу над зачетным заданием

--- Зачет/main.py
+++ Зачет/main.py
@@ -1,4 +1,8 @@
-# TODO: Создайть генераторы для создания продуктов
+import random
+from typing import Union
+
+from food_generator import gen_food
+
 
 class IdCounter:
     id_generator = 0
@@ -14,11 +18,30 @@ class Password:
 
 
 class Product:
-    def __init__(self, name, price, rating):
-        self.__id = None  # TODO: При инициализации должен сам определяться.
+    """Класс, описывающий продукт питания. """
+
+    def __init__(self, name: str, price: Union[int, float], rating: Union[int, float]):
+        """
+        Инициализация объекта "Продукт питания"
+
+        :param name: Название продукта питания
+        :param price: Цена продукта питания
+        :param rating: Рейтинг продукта питания
+        """
+        self.__id = self.id_init()
+
+        self.is_valid_name(name)
         self.__name = name
+
+        self.is_valid_int_or_float(price)
         self._price = price
+
+        self.is_valid_int_or_float(rating)
         self._rating = rating
+
+    @staticmethod
+    def id_init():
+        return random.randint(1, 1000)
 
     @property
     def id(self):
@@ -45,19 +68,31 @@ class Product:
         self._rating = rating
 
     def __str__(self):
-        return f"{self.id}_{self.__name}"
+        return f"{self.__id}_{self.__name}"
 
     def __repr__(self):
-        return f"{__class__.__name__}({self.__name}, {self.price}, {self.rating})"
+        return f"{__class__.__name__}({self.__name}, {self._price}, {self._rating})"
+
+    @staticmethod
+    def is_valid_name(name):
+        if not isinstance(name, str):
+            raise TypeError(f"Неверный тип данных: {name}, ожидается str")
+
+    @staticmethod
+    def is_valid_int_or_float(number):
+        if not isinstance(number, (int, float)):
+            raise TypeError(f"Неверный тип данных: {number}, ожидается int")
+        if number < 0:
+            raise ValueError(f"Неверное значение: {number}, ожидается > 0")
 
 
 class Cart:
     def __init__(self, product_list):
         self.product_list = product_list
-    
+
     def add_product(self):
         pass
-    
+
     def del_product(self):
         pass
 
@@ -65,14 +100,14 @@ class Cart:
 class User(Cart):
     def __init__(self, username, password, product_list):
         super().__init__(product_list)
-        
+
         self.id = None  # TODO: При инициализации должен сам определяться.
         self.__username = username
         self.password = password
-        
+
     def __str__(self):
         pass
-    
+
     def __repr__(self):
         pass
 
@@ -83,4 +118,6 @@ class Store(User):
 
 
 if __name__ == '__main__':
-    pass
+    a = Product('apple', 9, 3.0)
+    a.price = 33333
+    print(a.__dict__)
